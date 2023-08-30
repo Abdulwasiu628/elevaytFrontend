cba6f4b695ececddf1fe1c2c89df10b5df151849
shittu wasiu <abdulwasiushittu1416@gmail.com>
2023-08-31 02:34:09 +0800

    apple pay method done

--- elevayt/src/components/ApplePay.js
+++ elevayt/src/components/ApplePay.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState } from "react";
 import { useStripe, useElements, PaymentRequestButtonElement } from "@stripe/react-stripe-js";
 import PropTypes from "prop-types";
+import { postApis } from "../data/getApis";
 const ApplePayButton = ({payment}) => {
   
   const stripe = useStripe();
   const elements = useElements();
   const [paymentRequest, setPaymentRequest] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
   console.log("payment ", payment.currency.toLowerCase());
   useEffect(() => {
     // Include the Apple Pay button using the Apple Pay JS API
@@ -31,6 +33,29 @@ const ApplePayButton = ({payment}) => {
     }).catch((err) => {
       if(err){
         console.log("Error:", err);
+      }
+    });
+    pr.on("paymentmethod", async(e) => {
+      postApis("https://elevayt.onrender.com/pay/create-payment-intent", payment)
+        .then((result) => {
+          setClientSecret(result.clientSecret);
+        })
+        .catch((err) => {
+          return err;
+        });
+      const {error, paymentIntent} = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: e.paymentMethod.id
+      },
+      {
+        handleActions: false
+      }
+      );
+      if(error){
+        e.complete("fail");
+      }
+      e.complete("success");
+      if(paymentIntent.status == "requires_action"){
+        stripe.confirmCardPayment(clientSecret);
       }
     });
   }, [stripe, elements]);
